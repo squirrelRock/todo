2f378ed0db5e920133071c287242d3aa668727f6
Kelly Durkin <kdurkin@bearcubs.santarosa.edu>
2024-10-10 14:20:57 -0700

    Wrap app with Chakra

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
 import "@/styles/globals.css";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+
+
+import { ChakraProvider } from "@chakra-ui/react";
+function MyApp({ Component, pageProps }) {
+return (
+<ChakraProvider>
+<Component {...pageProps} />
+</ChakraProvider>
+);
 }
+export default MyApp;
