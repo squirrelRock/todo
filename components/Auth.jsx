0517956470b3a133d11e72a1246adf99ff0c45bf
Kelly Durkin <kdurkin@bearcubs.santarosa.edu>
2024-10-16 09:21:47 -0700

    fix auth.jsx and useAuth.js

--- components/Auth.jsx
+++ components/Auth.jsx
@@ -4,50 +4,69 @@ import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { FaGoogle, FaMoon, FaSun } from "react-icons/fa";
 import { auth } from "../firebase";
 import useAuth from "../hooks/useAuth";
+
+// react jsx login component
 const Auth = () => {
-const { toggleColorMode, colorMode } = useColorMode();
-const { isLoggedIn, user } = useAuth();
-const handleAuth = async () => {
-const provider = new GoogleAuthProvider();
-signInWithPopup(auth, provider)
-.then((result) => {
-// This gives you a Google Access Token. You can use it to access the Google API.
-const credential = GoogleAuthProvider.credentialFromResult(result);
-const token = credential.accessToken;
-// The signed-in user info.
-const user = result.user;
-// ...
-})
-.catch((error) => {
-// Handle Errors here.
-const errorCode = error.code;
-const errorMessage = error.message;
-// The email of the user's account used.
-const email = error.customData.email;
-// The AuthCredential type that was used.
-const credential = GoogleAuthProvider.credentialFromError(error);
-// ...
-});
+    const { toggleColorMode, colorMode } = useColorMode();
+    const { isLoggedIn, user } = useAuth() || {};
+    // define a function to perform the login operation
+    const handleAuth = async () => {
+        const provider = new GoogleAuthProvider();
+        // some async calls with promise
+        signInWithPopup(
+            auth,
+            provider
+        ).then(
+            // since we got a promise, inside the then we get results returns
+            (result) => {
+                console.log(result);
+                // This gives you a Google Access Token. You can use it to access the Google API.
+                const credential = GoogleAuthProvider.credentialFromResult(result);
+                const token = credential.accessToken;
+                // now we should be able to get info about the user who is logged in!
+                const user = result.user;
+            }
+        ).catch(
+            (error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                const email = error.customData.email;
+                const credential = GoogleAuthProvider.credentialFromError(error);
+                console.log("authentication error " + errorCode + errorMessage);
+            }
+        );
+    };
+    // define the jsx component
+    return (
+        <Box display="flex" alignItems="center" justifyContent="space-between">
+            <Box>
+                <Link href="/add-todo">Add To Do</Link>
+            </Box>
+            <Box>
+            <Link href="/">List All To Dos</Link>
+            </Box>
+            <Box textAlign="right">
+                <Button onClick={() => toggleColorMode()}>
+                    {colorMode == "dark" ? <FaSun /> : <FaMoon />}
+                </Button>
+                {" "}
+                {isLoggedIn && (
+                    <>
+                        <Text color="green.500">{user.email}</Text>
+                        <Link color="red.500" onClick={ () => auth.signOut() }>
+                            Logout
+                        </Link>
+                    </>
+                )}
+                {!isLoggedIn && (
+                    <Button leftIcon={<FaGoogle />} onClick={ () => handleAuth() }>
+                        Login with Google
+                    </Button>
+                )}
+            </Box>
+        </Box>
+    );
 };
-return (
-<Box position={"fixed"} top="5%" right="5%">
-<Button onClick={() => toggleColorMode()}>
-{colorMode == "dark" ? <FaSun /> : <FaMoon />}
-</Button>{" "}
-{isLoggedIn && (
-<>
-<Text color="green.500">{user.email}</Text>
-<Link color="red.500" onClick={() => auth.signOut()}>
-Logout
-</Link>
-</>
-)}
-{!isLoggedIn && (
-<Button leftIcon={<FaGoogle />} onClick={() => handleAuth()}>
-Login with Google
-</Button>
-)}
-</Box>
-);
-};
+
+// export component!
 export default Auth;
--- hooks/useAuth.js
+++ hooks/useAuth.js
@@ -1,14 +1,31 @@
+// import 2 react functions to implement react hooks with effect and state
 import { useEffect, useState } from "react";
 import { auth } from "../firebase";
+
+// uses react hooks, learn at: https://reactjs.org/docs/hooks-intro.html
 const useAuth = () => {
-const [user, setUser] = useState(null);
-const [isLoggedIn, setIsLoggedIn] = useState(false);
-useEffect(() => {
-auth.onAuthStateChanged((user) => {
-setIsLoggedIn(user && user.uid ? true : false);
-setUser(user);
-});
-});
-return { user, isLoggedIn };
-};
+    // ask react to define a state variable and an associated function to change its value
+    const [ user, setUser ] = useState(null);
+    const [ isLoggedIn, setIsLoggedIn ] = useState(false);
+    // ask react to manage our state variables based on a block of code we give it to run
+    useEffect(
+        // we are passing an anonymous arrow function to react's useEffect()
+        () => {
+            auth.onAuthStateChanged(
+                // we are passing another anonymous function to firebase's onAuthStateChanged()
+                (user) => {
+                    // with the user object value that firebase returns 
+                    // set react state variable isLoggedIn 
+                    setIsLoggedIn( user && user.uid ? true : false );
+                    // set react state variable user
+                    setUser( user );
+                }
+            );
+        }
+    );
+    // remember to return something for this useAuth() function we call for authentication
+    return { user, isLoggedIn };
+}
+
+// don't forget to export the function we made here so other files can import it!
 export default useAuth;
